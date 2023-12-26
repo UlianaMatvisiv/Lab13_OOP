12da5e764161ce02408ac7cdb5331dc4bbdf9d15
UlianaMatvisiv <116552600+UlianaMatvisiv@users.noreply.github.com>
2023-12-26 22:15:56 +0200

    fourth

--- src/main/java/ucu/edu/ua/taskthree/OpenAIEnricher.java
+++ src/main/java/ucu/edu/ua/taskthree/OpenAIEnricher.java
@@ -12,28 +12,36 @@ import java.util.Scanner;
 public class OpenAIEnricher {
     @SneakyThrows
     public static void main(String[] args) throws IOException {
-        URL url_get = new URL("https://nltu.edu.ua/");
-        HttpURLConnection connection_get = (HttpURLConnection) url_get.openConnection();
-        connection_get.setRequestMethod("GET");
-        connection_get.connect();
-        String text_get = new Scanner(connection_get.getInputStream()).useDelimiter("\\Z").next();
-        System.out.println(text_get);
+        URL urlGet = new URL("https://nltu.edu.ua/");
+        HttpURLConnection connectionGet = (HttpURLConnection) urlGet
+                .openConnection();
+        connectionGet.setRequestMethod("GET");
+        connectionGet.connect();
+        String textGet = new Scanner(connectionGet.getInputStream())
+                .useDelimiter("\\Z").next();
+        System.out.println(textGet);
 
-        URL url_post = new URL("https://api.openai.com/v1/completions");
-        HttpURLConnection connection_post = (HttpURLConnection) url_post.openConnection();
-        connection_post.setRequestMethod("POST");
-        connection_post.setRequestProperty("Content-Type", "application/json");
-        connection_post.setRequestProperty("Authorization", "Bearer my api key");
+        URL urlPost = new URL("https://api.openai.com/v1/completions");
+        HttpURLConnection connectionPost = (HttpURLConnection) urlPost
+                .openConnection();
+        connectionPost.setRequestMethod("POST");
+        connectionPost.setRequestProperty("Content-Type",
+                 "application/json");
+        connectionPost.setRequestProperty("Authorization",
+                 "Bearer my api key");
 
         JSONObject data = new JSONObject();
         data.put("model", "gpt-3.5-turbo");
-        data.put("prompt", "Extract logo from " + text_get);
+        data.put("prompt", "Extract logo from " + textGet);
 
-        connection_post.setDoOutput(true);
-        connection_post.getOutputStream().write(data.toString().getBytes(StandardCharsets.UTF_8));
+        connectionPost.setDoOutput(true);
+        connectionPost.getOutputStream()
+                .write(data.toString()
+                .getBytes(StandardCharsets.UTF_8));
 
-        String text_post = new Scanner(connection_post.getInputStream(), StandardCharsets.UTF_8)
+        String textPost = new Scanner(connectionPost.getInputStream(), 
+                StandardCharsets.UTF_8)
                 .useDelimiter("\\Z").next();
-        System.out.println(text_post);
+        System.out.println(textPost);
     }
 }
